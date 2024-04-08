bfb2541b6c5e8bad67b6509df123175cbd4e8ec1
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-04-08 16:22:53 -0400

    Made card widgets render dynamic amounts

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,22 +1,31 @@
 import { PieChart } from '@mui/x-charts/PieChart';
+import { Gauge } from '@mui/x-charts/Gauge';
 import './Card.css';
 
 function Card() {
   return (
     <div className='card'>
-      <PieChart
-        series={[
-          {
-            data: [
-              { id: 0, value: 10 },
-              { id: 1, value: 15 },
-              { id: 2, value: 20 },
-            ],
-          },
-        ]}
-        width={250}
-        height={150}
-      />
+      {Math.random() > 0.3 ? (
+        <PieChart
+          series={[
+            {
+              data: [
+                { id: 0, value: Math.random() * 20 },
+                { id: 1, value: Math.random() * 20 },
+                { id: 2, value: Math.random() * 20 },
+              ],
+            },
+          ]}
+          width={250}
+          height={150}
+        />
+      ) : (
+        <Gauge
+          width={100}
+          height={100}
+          value={Math.round(Math.random() * 100)}
+        />
+      )}
     </div>
   );
 }
