d2f078fb7eeac7cbb62750f77a883c10e457dbe8
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-04-08 16:21:38 -0400

    Cleaned up pseudocode and changed title element on App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,8 +5,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 function App() {
   return (
     <>
-      <h1>Error Boundaries are Friends</h1>
-      {/* Note: wrapping the entire app in an error boundary is not best-practice */}
+      <h1>Error Boundaries in Practice</h1>
       <ErrorBoundary fallback={<div>Oops! The whole dashboard crashed</div>}>
         <Dashboard />
       </ErrorBoundary>
